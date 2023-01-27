8164f430f4c0f665385c2f900a9e158ea4266e67
KenYu <spiderkiller6@gmail.com>
2023-01-27 16:14:56 +0800

    asdf

--- retinanet/config_train.py
+++ retinanet/config_train.py
@@ -1,15 +1,15 @@
-DEVICE = 'cuda:1' # 'cuda:1', 'cuda:0' 
+DEVICE = 'cuda:0' # 'cuda:1', 'cuda:0' 
 
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 NUMBER_WORKERS = 8
 NUM_EPOCH = 100
-VALID_EPOCH = 9999 # 5
+VALID_EPOCH = 1 # 5
 SAVE_EPOCH  = 5 # 5
-EXP_NAME = "2D_detection_crop_feature_map_fuse_16"
+EXP_NAME = "2D_detection_fine_tune"
 # TODO all_feature_map currently has no use
-BACKBONE = "crop_feature_map_fuse_16" # "original" 'crop_feature_map' 'all_feature_map'
+BACKBONE = "original" # "original" 'crop_feature_map' 'all_feature_map' 'crop_feature_map_fuse_16'
 
 CATEGORY = ['Car']
 OUTPUT_DIR = f"checkpoint/{EXP_NAME}"
-PATH_TO_WEIGHTS = "/home/lab530/KenYu/pytorch-retinanet/checkpoint/2D_detection_2/epoch29.pt"
-SPLIT_PATH = "/home/lab530/KenYu/visualDet3D/visualDet3D/data/kitti/chen_split/" # "only_one_split " "dummy_exp" "chen_split" "debug_split"
+PATH_TO_WEIGHTS = "/home/lab530/KenYu/pytorch-retinanet/checkpoint/2D_detection_original_pretrain.pt"
+SPLIT_PATH = "/home/lab530/KenYu/visualDet3D/visualDet3D/data/kitti/debug_split/" # "only_one_split " "dummy_exp" "chen_split" "debug_split"
